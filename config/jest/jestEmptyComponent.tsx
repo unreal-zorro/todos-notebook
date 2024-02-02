23e8467939d7c579d84b3575a2c13b74c3d3df1f
unreal-zorro <unrealzorro@gmail.com>
2024-02-02 19:10:08 +0300

    add jest

---
+++ config/jest/jestEmptyComponent.tsx
@@ -0,0 +1,7 @@
+import React, { type ReactNode } from 'react';
+
+const jestEmptyComponent = function (): ReactNode {
+  return <div />;
+};
+
+export default jestEmptyComponent;
--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { App } from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  test('Render Hello world!', () => {
+    render(<App />);
+    expect(screen.getByText('Hello, world!')).toBeInTheDocument();
+  });
 });
