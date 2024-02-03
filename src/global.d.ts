f0adc622bf4016dfab6f87a68d659bb46668f7d4
unreal-zorro <unrealzorro@gmail.com>
2024-02-03 16:04:34 +0300

    global.d.ts update

--- src/global.d.ts
+++ src/global.d.ts
@@ -10,10 +10,24 @@ declare module '*.png';
 declare module '*.jpg';
 declare module '*.jpeg';
 declare module "*.svg" {
-  import React from "react";
-  const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
+  import type React from 'react';
+  const SVG: React.FC<React.SVGProps<SVGSVGElement>>;
   export default SVG;
 }
+declare module 'storybook-addon-mock';
 
+declare const __IS_DEV__: boolean;
+declare const __API__: string;
+declare const __PROJECT__: 'storybook' | 'frontend' | 'jest';
 declare const __PLATFORM__: 'desktop' | 'mobile';
 declare const __ENV__: 'production' | 'development';
+
+type DeepPartial<T> = T extends object
+  ? {
+      [P in keyof T]?: DeepPartial<T[P]>;
+    }
+  : T;
+
+type OptionalRecord<K extends keyof any, T> = {
+  [P in K]?: T;
+};
